4c5067b995675d9f1e8c013fc5e898f64e828352
Sem Postma <sem.postma@gmail.com>
2019-08-16 19:36:47 +0200

    various

--- index.js
+++ index.js
@@ -4,38 +4,25 @@ const path = require('path');
 
 const package = require('./package.json');
 
+const { findFilesRecusively } = require('./lib/files');
+const { doChecks } = require('./lib/cordova/check');
+
 program
     .version(package.version)
     .name(package.name)
     .description(package.description);
 
 program
-    .command('backup')
-    .option('-d, --depth <number>', 'Depth to search for android .jks files', parseInt)
+    .command('accumulate')
+    .option('-d, --depth <number>', 'Depth to search for files', parseInt)
     .option('-o, --out <path>', 'Output path', path => path)
     .option('-e, --extensions <extensions>', 'Extensions to backup', extensions => (extensions || '').split(/\s*,\s*/))
     .option('-i, --ignore <ignore>', 'Directories to ignore', ignore => (ignore || '').split(/\s*,\s*/))
-    .action(async function({ depth, out, extensions = ['jks'], ignore = ['node_modules', '.git'] }) {
-
-        const recurse = async (parent = './', depth = 3) => {
-            const files = [];
-            const dirs = await fs.readdir(parent);
-            await Promise.all(dirs.map(async dir => {
-                dir = path.resolve(parent, dir);
-                const stat = await fs.lstat(dir);
-                if (depth > 0 && stat.isDirectory() && stat.isSymbolicLink() === false && ignore.every(d => dir !== d)) {
-                    files.push(...await recurse(dir, depth - 1))
-                } else if (stat.isFile() && extensions.some(ext => dir.endsWith(`.${ext}`))) {
-                    files.push(dir)
-                }
-            }));
+    .action(async function({ depth, out, extensions = ['jks'], ignore = ['node_modules', '.git', 'bower_components', ] }) {
         
-            return files;
-        }
+        if (!depth || !out) console.error('"depth" and "out" have to be set.') || process.exit(1);
 
-        if (!depth || !out) console.error('"depth" and "out" have to be set.') || process.exit(1)
-
-        const filesToBackup = await recurse('./', depth);
+        const filesToBackup = await findFilesRecusively({ ignore, extensions, depth });
 
         filesToBackup.forEach(file => {
             console.log(`backing up "${file}"`);
@@ -46,9 +33,11 @@ program
         console.log(`Done! Backed up ${filesToBackup.length} files.`);
     })
 
-
-
-    
+program
+    .command('cordova:check')
+    .action(async function() {
+        doChecks({ cwd: process.cwd() })
+    })
 
 program.parse(process.argv);
 
